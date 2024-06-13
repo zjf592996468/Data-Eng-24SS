48443a3ad626ec0d572b7f3e5d12c72dc71198a6
Jifeng Zhou <zjf592996468@126.com>
2024-06-13 21:31:47 +0200

    appox finish task02
    用了jaccard相似度
    但是用于判断相似度的attribute只使用了authors和title，可能还要修改补充
    阈值设置为0.5
    结果以数组形式保存为txt
    和官方给的DPL似乎还有差别

--- 02_Entity Resolution/entity.py
+++ 02_Entity Resolution/entity.py
@@ -15,27 +15,49 @@ def preprocess_data(df):
 data = preprocess_data(data)
 
 
-# calculate similarity
-def cal_sim(df):
-    # todo: calculate similarity - cos, jaccard
-    return sim
+# # calculate cosine similarity
+# def cos_sim(df):
+#     # todo: calculate cos similarity
+#
+#     return cos_sim
+#
+#
+# sim_matrix = cos_sim(data)
 
 
-sim_matrix = cal_sim(data)
+# calculate jaccard similarity
+def jaccard_sim(str1, str2):
+    set1 = set(str1.split())
+    set2 = set(str2.split())
+    intersection = len(set1.intersection(set2))
+    union = len(set1.union(set2))
+    return intersection / union if union != 0 else 0
 
 
 # check duplicate
-def check_duplicate(df):
+def check_duplicate(df, threshold=0.5):
     duplicates = []
+    print("start checking duplicates...")
     # todo: detect all duplicates according to sim_matrix
+    for i in range(len(df)):
+        for j in range(i + 1, len(df)):
+            str1 = df.iloc[i]['authors'] + ' ' + df.iloc[i]['title']  # 感觉这里还不太够，只判断作者和题目
+            str2 = df.iloc[j]['authors'] + ' ' + df.iloc[j]['title']
+            sim = jaccard_sim(str1, str2)
+            if sim > threshold:
+                duplicates.append((i + 1, j + 1))
     return duplicates
 
 
 t_start = time.perf_counter()
-duplicate_pairs = check_duplicate(sim_matrix)
+duplicate_pairs = check_duplicate(data)
 t_end = time.perf_counter()
 
 # save as txt
+with open('duplicates.txt', 'w') as file:
+    for pair in duplicate_pairs:
+        file.write(f"{pair}\n")
+    file.close()
 
 # record runtime
 runtime = t_end - t_start
