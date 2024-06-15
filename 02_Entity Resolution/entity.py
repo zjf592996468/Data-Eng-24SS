f43d042bda2bdcb37f44a5019491e59746ded422
Jifeng Zhou <zjf592996468@126.com>
2024-06-15 22:11:31 +0200

    finish task02
    完成program任务02，使用了blocking方法，并对结果进行评估
    上传duplicate结果
    上传requirements
    运行结果：
    Runtime: 318.2090 seconds
    Precision: 0.4555, Recall: 0.2260, F-measure: 0.3021

--- 02_Entity Resolution/entity.py
+++ 02_Entity Resolution/entity.py
@@ -1,64 +1,173 @@
+import ast
+import re
 import time
+from collections import defaultdict
 
+import nltk
+import numpy as np
 import pandas as pd
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+
+# download nltk stopwords
+nltk.download('stopwords')
 
 # load dataset
 data = pd.read_csv('cora.tsv', sep='\t')
+
+# union nltk and sklearn stopwords
+stop_words = set(stopwords.words('english')).union(set(ENGLISH_STOP_WORDS))
+
+
+# remove all special characters
+def remove_special_characters(text):
+    return re.sub(r'[^A-Za-z0-9\s]', '', text.lower())
 
 
 # preprocess dataset
 def preprocess_data(df):
     df.fillna('', inplace=True)  # fill NAN with blank
+    df = df.astype(str).map(remove_special_characters)  # remove special characters
+
     return df
 
 
 data = preprocess_data(data)
 
 
-# # calculate cosine similarity
-# def cos_sim(df):
-#     # todo: calculate cos similarity
-#
-#     return cos_sim
-#
-#
-# sim_matrix = cos_sim(data)
+# tokenize text
+def tokenize(text):
+    # uniform all letters to lowercase and split them into a set
+    tokens = text.split()
+    tokens = set(token for token in tokens if token not in stop_words)
+
+    return tokens
+
+
+# create blocks by attributes in dataset
+def create_blocks(data, attributes):
+    blocks = defaultdict(set)
+    for index, row in data.iterrows():
+        for attr in attributes:
+            text = row[attr]
+            tokens = tokenize(text)
+            for token in tokens:
+                blocks[token].add(row['id'])
+
+    return blocks
+
+
+# filter blocks by the size of the block
+def filter_blocks(blocks, min_size=2, max_size=100):
+    filtered_blocks = {token: ids for token, ids in blocks.items() if min_size <= len(ids) <= max_size}
+
+    return filtered_blocks
+
+
+# create inverted index without 'id'
+attributes_list = data.columns.drop('id').tolist()
+
+# create and filter blocks
+blocks = create_blocks(data, attributes_list)
+blocks = filter_blocks(blocks)
+
+
+# get all probable pairs
+def prob_pairs(blocks):
+    pairs = set()
+    for token, ids in blocks.items():
+        ids_list = list(ids)
+        for i in range(len(ids)):
+            for j in range(i + 1, len(ids)):
+                if ids_list[i] != ids_list[j]:
+                    pairs.add((ids_list[i], ids_list[j]))
+
+    return pairs
+
+
+pairs = prob_pairs(blocks)
+
+
+# get values of attributes
+def get_values(data, id1, id2, attr_list):
+    pair_value1 = ' '.join(data.loc[data['id'] == id1, attr_list].values[0])
+    pair_value2 = ' '.join(data.loc[data['id'] == id2, attr_list].values[0])
+
+    return pair_value1, pair_value2
 
 
 # calculate jaccard similarity
 def jaccard_sim(str1, str2):
-    set1 = set(str1.split())
-    set2 = set(str2.split())
+    set1 = tokenize(str1)
+    set2 = tokenize(str2)
     intersection = len(set1.intersection(set2))
     union = len(set1.union(set2))
+
     return intersection / union if union != 0 else 0
 
 
 # check duplicate
-def check_duplicate(df, threshold=0.5):
-    duplicates = []
+def check_duplicate(data, pairs, threshold=0.5):
+    duplicates = set()
     print("start checking duplicates...")
-    # todo: detect all duplicates according to sim_matrix
-    for i in range(len(df)):
-        for j in range(i + 1, len(df)):
-            str1 = df.iloc[i]['authors'] + ' ' + df.iloc[i]['title']  # 感觉这里还不太够，只判断作者和题目
-            str2 = df.iloc[j]['authors'] + ' ' + df.iloc[j]['title']
-            sim = jaccard_sim(str1, str2)
-            if sim > threshold:
-                duplicates.append((i + 1, j + 1))
+    for pair in pairs:
+        author = get_values(data, pair[0], pair[1], ['authors'])
+        title = get_values(data, pair[0], pair[1], ['booktitle', 'journal', 'title'])
+        date = get_values(data, pair[0], pair[1], ['date', 'month', 'year'])
+        sim = np.array(
+            [jaccard_sim(author[0], author[1]), jaccard_sim(title[0], title[1]), jaccard_sim(date[0], date[1])])
+        if np.mean(sim) > threshold:
+            duplicates.add(pair)
+
     return duplicates
 
 
-t_start = time.perf_counter()
-duplicate_pairs = check_duplicate(data)
-t_end = time.perf_counter()
+# save and load results
+def save_detected_pairs(pairs, filename='duplicates.txt'):
+    with open(filename, 'w') as file:
+        for pair in pairs:
+            file.write(f"{pair}\n")
 
-# save as txt
-with open('duplicates.txt', 'w') as file:
-    for pair in duplicate_pairs:
-        file.write(f"{pair}\n")
-    file.close()
+
+def load_detected_pairs(filename='duplicates.txt'):
+    detected_pairs = set()
+    with open(filename, 'r') as file:
+        for line in file:
+            detected_pairs.add(tuple(map(int, ast.literal_eval(line.strip()))))
+
+    return detected_pairs
+
+
+# load true duplicates
+true_pairs_df = pd.read_csv('cora_DPL.tsv', sep='\t')
+true_pairs = set((row['id1'], row['id2']) for _, row in true_pairs_df.iterrows())
+
+
+# optional: evaluate result
+def evaluate(true_pairs, predicted_pairs):
+    tp = len(true_pairs & predicted_pairs)
+    fp = len(predicted_pairs - true_pairs)
+    fn = len(true_pairs - predicted_pairs)
+    precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+    recall = tp / (tp + fn) if (tp + fn) > 0 else 0
+    f_measure = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+
+    return precision, recall, f_measure
+
+
+# check duplicates
+t_start = time.perf_counter()
+duplicate_pairs = check_duplicate(data, pairs)
+t_end = time.perf_counter()
 
 # record runtime
 runtime = t_end - t_start
 print(f"Runtime: {runtime:0.4f} seconds")
+
+# save results as txt
+save_detected_pairs(duplicate_pairs)
+
+# evaluate results
+detected_pairs = load_detected_pairs()
+precision, recall, f_measure = evaluate(true_pairs, detected_pairs)
+print(f"Precision: {precision:0.4f}, Recall: {recall:0.4f}, F-measure: {f_measure:0.4f}")
